c6bca6d36525efc3c9f706913221fb22fa0c7ec4
Andrii Moroz <andriyko83701@gmail.com>
2023-03-21 22:59:43 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import * as ReactDomClient from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Header extends React.Component {
+  render() {
+    return <header> Header</header>;
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  render() {
+    return (
+      <div className="name">
+        <Header />
+        <h1> HEllo</h1>
+        <input onClick={this.inputClick}/>
+      </div>
+    );
+  }
+
+  inputClick() {console.log("Clicked")}
+}
+
+const root = ReactDomClient.createRoot(document.getElementById("root"));
+
+root.render(<App />);
