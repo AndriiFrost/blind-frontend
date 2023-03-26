5addfe7d3e00f07bbc93947c1e5a5ed6b60370fb
Andrii Moroz <andriyko83701@gmail.com>
2023-03-26 20:14:07 +0300

    added state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from "react";
 import Header from "./components/Header";
 import Image from "./components/Image";
-import logo from './img/logo.png';
-import './css/main.css';
-
-
+import logo from "./img/logo.png";
+import "./css/main.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      helpText: "HelpHext",
+      userData: "",
+    };
+
+    this.inputClick = this.inputClick.bind(this)
+  }
+
   render() {
     return (
       <div className="name">
-        <Header title = "Hellooooooo"/>
-        <h1> HEllo</h1>
-        <input onClick={this.inputClick}/>
-        <Image image ={logo}/>
+        <Header title="Hellooooooo" />
+        <h1> {this.state.helpText}</h1>
+        <h1> {this.state.userData}</h1>
+        <input onClick={this.inputClick} 
+          onChange ={event => this.setState({userData: event.target.value})}
+        />
+        <Image image={logo} />
       </div>
     );
   }
 
-  inputClick() {console.log("Clicked")}
+  inputClick() {
+    this.setState({helpText: "ssss"});
+    console.log("Clicked");
+  }
 }
 
-export default App
+export default App;
